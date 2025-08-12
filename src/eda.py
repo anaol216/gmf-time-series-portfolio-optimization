5318ae79b24f77e67f9db35e0696fdd98bb82bd9
anaol216 <atinafuanaol@gmail.com>
2025-08-12 23:04:24 +0300

    new file:   src/eda.py

---
+++ src/eda.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+def plot_prices(df, save_path=None):
+    plt.figure(figsize=(14, 7))
+    for column in df.colums:
+        plt.plot(df.index, df[column], label=column)
+    plt.title('Adjusted Close Prices')    
+    plt.legend()
+    if save_path:
+        plt.savefig(save_path, dpi=300) 
+    plt.show()
+def plot_correlation_heatmap(df, save_path=None):
+    plt.figure(figsize=(6, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+    plt.title('Correlation Heatmap')
+    if save_path:
+        plt.savefig(save_path, dpi=300)
+    plt.show()        
